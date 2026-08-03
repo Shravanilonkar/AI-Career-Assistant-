5b58efe75fc210b0cec8f3f9aedc83a68d4e7e95
Shravani Lonkar <lonkarshravani@gmail.com>
2026-08-03 19:55:42 +0530

    Use Groq instead of Ollama

--- llm.py
+++ llm.py
@@ -1,16 +1,11 @@
-from langchain_ollama import ChatOllama
+import streamlit as st
+from langchain_groq import ChatGroq
 
-from config import LLM_MODEL
 
-
-def load_llm():
-    """
-    Load Ollama LLM.
-    """
-
-    llm = ChatOllama(
-        model=LLM_MODEL,
-        temperature=0
+def get_llm():
+    llm = ChatGroq(
+        model="llama-3.1-8b-instant",
+        temperature=0,
+        api_key=st.secrets["GROQ_API_KEY"],
     )
-
     return llm
